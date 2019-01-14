fde2a32710e074c065f0b22da110389cf407ae48
Stas Vlasenko <stasvlasenko@gmail.com>
2019-01-15 02:28:23 +0400

    Работали с путями в файле json_parse

--- Parse_json/json_parse.py
+++ Parse_json/json_parse.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 from pprint import pprint
@@ -5,11 +6,31 @@ from pprint import pprint
 with open("windows-1251.json", "r") as file:
      load_info = json.load(file)
 
-pprint(load_info)
+#print(load_info)
+memory = load_info['TextBody']
 
-#resopnse = requests.get("https://jsonplaceholder.typicode.com/todos")
-#todos = json.loads(resopnse.text)
+print(memory)
 
-#print(type(todos))
-#pprint(todos)
+memory = memory.lower()
+memory = memory.replace(',', '')
+memory = memory.replace('.', '')
+memory = memory.replace('/', '')
+memory = memory.replace('!', '')
+memory = memory.split()
 
+my_dict = {}
+for i in memory:
+    if len(i) > 2:
+        my_dict[i] = memory.count(i)
+
+my_dict = sorted(my_dict.items(), key = lambda x: x[1], reverse= True)
+
+for i, o in my_dict:
+    if o > 1:
+        print('%s - %s' % (i, o))
+
+
+directory = os.getcwd()
+print(directory)
+os.chdir('../')
+print(os.getcwd())
