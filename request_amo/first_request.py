c6d6a06be015c0c1f7debfb94efdf8d76ab31e86
Stas Vlasenko <stasvlasenko@gmail.com>
2019-01-18 00:54:00 +0400

    Разобрались с датами, надо только отфильтровать сделки

--- request_amo/first_request.py
+++ request_amo/first_request.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from pprint import pprint
+import datetime
 
 domain = 'izan'
 
@@ -19,37 +20,56 @@ def autorized(domain, setting):
     respond = requests.post(url, json=dict)
     #print(respond.cookies)
     return respond
-
+def list_account(domain, cookies):
+    method = 'api/v2/account'
+    url = 'https://%s.amocrm.ru/%s' % (domain, method)
+    params = {'with': 'note_types'}
+    r = requests.get(url, cookies=cookies)
+    return r
 def users(domain, cookies):
-    autorized(domain)
     method = 'api/v2/account'
     url = 'https://%s.amocrm.ru/%s' % (domain, method)
     params = 'with=users'
     r = requests.get(url, params=params, cookies=cookies)
     return r
-
 def list_funnel(domain, cookie):
     method = 'api/v2/pipelines'
     url = 'https://%s.amocrm.ru/%s' % (domain, method)
     #id = {'id': id_voronki}
     r = requests.get(url, cookies=cookies)
     return r
-
 def list_leads(domain, cookie):
+    # Показать список лидов в вороке и этапе, с временм изменения таким то
+    # Изменить у этих лидов этап
     method = 'api/v2/leads'
     url = 'https://%s.amocrm.ru/%s' % (domain, method)
-    id = {'id': 123123}
-    r = requests.get(url, params=id, cookies=cookies)
+    params = {'limit_rows': 2,
+              'status': 19123065}
+    r = requests.get(url, params=params, cookies=cookies)
     return r
+
 
 #Авторизуемся и сохраняем куки для дальнейших вызовов
 cookies = autorized(domain, setting).cookies
 
 #dict_users = users(domain, cookies).json()
 
+#dict_account = list_account(domain, cookies).json()
 dict_funnel = list_funnel(domain, cookies).json()
 dict_leads = list_leads(domain, cookies).json()
 
 print('Данные о воронках:')
 for i in dict_funnel['_embedded']['items']:
     print(dict_funnel['_embedded']['items'][i])
+
+print('Массив лидов:')
+#for i in dict_leads['_embedded']['items']:
+#    print(dict_leads['_embedded']['items'][i])
+
+pprint(dict_leads['_embedded']['items'])
+
+#Из даты в таймштамп
+start = datetime.datetime(2018,12,19,17,30,15).timestamp()
+stop = datetime.datetime(2018,12,19,19,30,15).timestamp()
+search_data = datetime.datetime.fromtimestamp(1545202333)
+print('Дата старта %s дата финиша %s, базовая дата:%s' %(start, stop, search_data))
