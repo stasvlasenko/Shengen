a9878ffac86eaf59ed41e01f888a9c42a5f896e1
Stas Vlasenko <stasvlasenko@gmail.com>
2019-01-21 02:21:43 +0400

    АМО срм не отвечает как надо

--- request_amo/first_request.py
+++ request_amo/first_request.py
@@ -4,7 +4,6 @@ from pprint import pprint
 import datetime
 
 domain = 'izan'
-
 with open('setting.json') as login:
     setting = json.load(login)
 
@@ -32,40 +31,47 @@ def users(domain, cookies):
     params = 'with=users'
     r = requests.get(url, params=params, cookies=cookies)
     return r
-def list_funnel(domain, cookie):
+def list_funnel(domain, cookies):
     method = 'api/v2/pipelines'
     url = 'https://%s.amocrm.ru/%s' % (domain, method)
     #id = {'id': id_voronki}
     r = requests.get(url, cookies=cookies)
     return r
-def list_leads(domain, cookie):
+def print_id_funnel(domain, cookies):
+    dict_funnel = list_funnel(domain, cookies).json()
+
+    for i in dict_funnel['_embedded']['items']:
+        print('Воронка: %s, id: %s' % (
+        dict_funnel['_embedded']['items'][i]['name'], dict_funnel['_embedded']['items'][i]['id']))
+        for ii in dict_funnel['_embedded']['items'][i]['statuses']:
+            print('Этап: %s, id: %s' % (dict_funnel['_embedded']['items'][i]['statuses'][ii]['name'],
+                                        dict_funnel['_embedded']['items'][i]['statuses'][ii]['id']))
+def list_leads(domain, cookies):
     # Показать список лидов в вороке и этапе, с временм изменения таким то
     # Изменить у этих лидов этап
     method = 'api/v2/leads'
     url = 'https://%s.amocrm.ru/%s' % (domain, method)
     params = {'limit_rows': 2,
-              'status': 19123065}
+              'status': 19123065,
+              }
     r = requests.get(url, params=params, cookies=cookies)
     return r
-
+def select_lead_time_update (dict_leads, time_start, time_end):
+    list_leads_in_time = []
+    for lead in dict_leads['_embedded']['items']:
+        print(lead['updated_at'])
+        if time_start <= lead['updated_at'] <= time_end:
+            list_leads_in_time.append(lead['id'])
+    return list_leads_in_time
 
 #Авторизуемся и сохраняем куки для дальнейших вызовов
 cookies = autorized(domain, setting).cookies
-
 #dict_users = users(domain, cookies).json()
-
 #dict_account = list_account(domain, cookies).json()
 dict_funnel = list_funnel(domain, cookies).json()
 dict_leads = list_leads(domain, cookies).json()
 
-print('Данные о воронках:')
-for i in dict_funnel['_embedded']['items']:
-    print(dict_funnel['_embedded']['items'][i])
-
 print('Массив лидов:')
-#for i in dict_leads['_embedded']['items']:
-#    print(dict_leads['_embedded']['items'][i])
-
 pprint(dict_leads['_embedded']['items'])
 
 #Из даты в таймштамп
@@ -73,3 +79,28 @@ start = datetime.datetime(2018,12,19,17,30,15).timestamp()
 stop = datetime.datetime(2018,12,19,19,30,15).timestamp()
 search_data = datetime.datetime.fromtimestamp(1545202333)
 print('Дата старта %s дата финиша %s, базовая дата:%s' %(start, stop, search_data))
+
+
+list_lead_time_update = select_lead_time_update(dict_leads, 1545202300, 1545202400)
+print(list_lead_time_update)
+print_id_funnel(domain, cookies)
+
+def update_lead_status (list_lead, change_status, domain, cookies):
+    method = 'api/v2/leads'
+    url = 'https://%s.amocrm.ru/%s' % (domain, method)
+    headers = {'Content-type': 'application/json',  # Определение типа данных
+           'Accept': 'text/plain',
+           'Content-Encoding': 'utf-8'}
+    data = {'update': [{
+                      'id': list_lead[0],
+                      'status_id': change_status
+                  }]
+            }
+    data = json.dumps(data)
+    print(data)
+
+    r = requests.post(url, data=data, cookies=cookies, headers=headers)
+    return r
+
+response = update_lead_status(list_lead_time_update, 23743182, domain, cookies)
+print(response.json())
