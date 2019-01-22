6c0e74cc7d1e31537d9154179b63a5902a8cca69
Stas Vlasenko <stasvlasenko@gmail.com>
2019-01-23 03:22:53 +0400

    Всё работает

--- request_amo/first_request.py
+++ request_amo/first_request.py
@@ -7,6 +7,7 @@ domain = 'izan'
 with open('setting.json') as login:
     setting = json.load(login)
 
+
 def autorized(domain, setting):
     dict = {
         'USER_LOGIN': setting['login'],
@@ -17,7 +18,7 @@ def autorized(domain, setting):
     url = 'https://%s.amocrm.ru/%s?type=json' % (domain, method)
 
     respond = requests.post(url, json=dict)
-    #print(respond.cookies)
+    # print(respond.cookies)
     return respond
 def list_account(domain, cookies):
     method = 'api/v2/account'
@@ -34,73 +35,113 @@ def users(domain, cookies):
 def list_funnel(domain, cookies):
     method = 'api/v2/pipelines'
     url = 'https://%s.amocrm.ru/%s' % (domain, method)
-    #id = {'id': id_voronki}
+    # id = {'id': id_voronki}
     r = requests.get(url, cookies=cookies)
-    return r
-def print_id_funnel(domain, cookies):
-    dict_funnel = list_funnel(domain, cookies).json()
-
+    return r.json()
+def print_id_funnel(dict_funnel):
     for i in dict_funnel['_embedded']['items']:
         print('Воронка: %s, id: %s' % (
-        dict_funnel['_embedded']['items'][i]['name'], dict_funnel['_embedded']['items'][i]['id']))
+            dict_funnel['_embedded']['items'][i]['name'], dict_funnel['_embedded']['items'][i]['id']))
         for ii in dict_funnel['_embedded']['items'][i]['statuses']:
             print('Этап: %s, id: %s' % (dict_funnel['_embedded']['items'][i]['statuses'][ii]['name'],
                                         dict_funnel['_embedded']['items'][i]['statuses'][ii]['id']))
-def list_leads(domain, cookies):
+def list_leads(domain, cookies, status):
     # Показать список лидов в вороке и этапе, с временм изменения таким то
     # Изменить у этих лидов этап
     method = 'api/v2/leads'
     url = 'https://%s.amocrm.ru/%s' % (domain, method)
-    params = {'limit_rows': 2,
-              'status': 19123065,
+    params = {'limit_rows': 100,
+              'status': int(status),
+              'filter[date_modify][from]': 1545742800,
+              'filter[date_modify][to]': 1545750000
               }
+
     r = requests.get(url, params=params, cookies=cookies)
     return r
-def select_lead_time_update (dict_leads, time_start, time_end):
-    list_leads_in_time = []
+def select_lead_time_update(dict_leads, time_start, time_end):
+    list_leads_in_time = {}
+    i = 0
     for lead in dict_leads['_embedded']['items']:
         print(lead['updated_at'])
         if time_start <= lead['updated_at'] <= time_end:
-            list_leads_in_time.append(lead['id'])
+            list_leads_in_time = {i: [{'id': lead['id'],
+                                       'name': lead['name'],
+                                       'updated_at': lead['updated_at'],
+                                       'status_id': 23743182
+
+                                       }]}
+            i += 1
     return list_leads_in_time
+def update_lead_status(dict_leads, domain, cookies):
+    #for i in dict_leads['_embedded']['items']:
+        method = 'api/v2/leads'
+        url = 'https://%s.amocrm.ru/%s' % (domain, method)
 
-#Авторизуемся и сохраняем куки для дальнейших вызовов
+        headers = {'Content-type': 'application/json',  # Определение типа данных
+                   'Accept': 'application/json',
+                   'Content-Encoding': 'utf-8'}
+        data = {'update': dict_leads['_embedded']['items']}
+        """
+        data = {'update': [{
+                    'id': i['id'],
+                    'name': i['name'],
+                    'updated_at': i['updated_at'],
+                    'status_id': i['status_id']
+        }]}
+        """
+
+        data = json.dumps(data)
+
+        r = requests.post(url, data=data, cookies=cookies, headers=headers)
+        print(r.json())
+
+# Авторизуемся и сохраняем куки для дальнейших вызовов
 cookies = autorized(domain, setting).cookies
-#dict_users = users(domain, cookies).json()
-#dict_account = list_account(domain, cookies).json()
-dict_funnel = list_funnel(domain, cookies).json()
-dict_leads = list_leads(domain, cookies).json()
 
-print('Массив лидов:')
-pprint(dict_leads['_embedded']['items'])
+# Получим список воронок и статусов, чтобы выбрать куда переносить
+#dict_funnel = list_funnel(domain, cookies)
+#print_id_funnel(dict_funnel)
 
-#Из даты в таймштамп
-start = datetime.datetime(2018,12,19,17,30,15).timestamp()
-stop = datetime.datetime(2018,12,19,19,30,15).timestamp()
+#Цикл который остановит скрипт, когда кончатся сделки
+list_status = True
+while list_status == True:
+    # Спрашиваем где выбирать сделки
+    print('Введите номер этапа, с которого забрать сделки: 19123065')
+    status = 19123065
+    dict_leads = list_leads(domain, cookies, status).json()
+
+    print('Количество сделок выбрано:', len(dict_leads['_embedded']['items']))
+    if len(dict_leads['_embedded']['items']) < 100:
+        list_status = False
+
+    for i in dict_leads['_embedded']['items']:
+        i['status_id'] = 23743182
+
+    update_lead_status(dict_leads, domain, cookies)
+
+
+
+
+
+"""
+# Из даты в таймштамп
+print('Выбираем интервал дат для лидов')
+start = datetime.datetime(2018, 12, 19, 17, 30, 15).timestamp()
+stop = datetime.datetime(2018, 12, 19, 19, 30, 15).timestamp()
 search_data = datetime.datetime.fromtimestamp(1545202333)
-print('Дата старта %s дата финиша %s, базовая дата:%s' %(start, stop, search_data))
+print('Дата старта %s дата финиша %s, базовая дата:%s' % (start, stop, search_data))
+"""
 
-
+"""
 list_lead_time_update = select_lead_time_update(dict_leads, 1545202300, 1545202400)
-print(list_lead_time_update)
-print_id_funnel(domain, cookies)
+print('финальный список тест')
+print(list_lead_time_update[1])
+#print_id_funnel(domain, cookies)
 
-def update_lead_status (list_lead, change_status, domain, cookies):
-    method = 'api/v2/leads'
-    url = 'https://%s.amocrm.ru/%s' % (domain, method)
-    headers = {'Content-type': 'application/json',  # Определение типа данных
-           'Accept': 'text/plain',
-           'Content-Encoding': 'utf-8'}
-    data = {'update': [{
-                      'id': list_lead[0],
-                      'status_id': change_status
-                  }]
-            }
-    data = json.dumps(data)
-    print(data)
 
-    r = requests.post(url, data=data, cookies=cookies, headers=headers)
-    return r
+
+
 
 response = update_lead_status(list_lead_time_update, 23743182, domain, cookies)
-print(response.json())
+pprint(response.json())
+"""
